98a0ce258b685377ce4206f8222b78e491a6ad7e
Brian Demers <bdemers@apache.org>
2025-06-12 12:29:03 -0400

    Initial version of code

---
+++ src/main/java/dev/diceroll/mcp/roller/McpRollerApplication.java
@@ -0,0 +1,36 @@
+package dev.diceroll.mcp.roller;
+
+import dev.diceroll.parser.Dice;
+import dev.diceroll.parser.ResultTree;
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+@SpringBootApplication
+public class McpRollerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McpRollerApplication.class, args);
+    }
+
+    @Bean
+    public ToolCallbackProvider tools(DiceNotationService diceService) {
+        return MethodToolCallbackProvider.builder()
+                .toolObjects(diceService)
+                .build();
+    }
+
+    @Service
+    public static class DiceNotationService {
+
+        @Tool(description = "Returns the results of a collection of dice")
+        public String rollDice(String diceNotation) {
+            ResultTree resultTree = Dice.detailedRoll(diceNotation);
+            return "Result of rolling dice: " + resultTree.getValue() + ", individual roll results were:\n\n" + Dice.debug(resultTree);
+        }
+    }
+}
